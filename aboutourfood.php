f28d988d3075bea4e49316bb91226f019a6ef8d4
kmuhammedafsal <mafsalk8@gmail.com>
2021-05-31 22:46:25 +0530

    m

---
+++ aboutourfood.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About Our Food</title>
+</head>
+<body>
+  <nav class="navbar navbar-expand-lg navbar-light ">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#"></a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <div class="container-fluid">
+            <a class="navbar-brand" href="#">
+              <img src="https://www.mcdonalds.com/content/dam/usa/nfl/assets/nav/arches-logo_108x108.jpg" alt="" width="140" height="100" class="d-inline-block align-top">
+              
+            </a>
+          </div>
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="menu.php">Menu</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="aboutourfood.php">Aboutus</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="signup.php">SignUp</a>
+          </li>
+        </ul>
+        <form class="d-flex">
+          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-success" type="submit">Search</button>
+        </form>
+      </div>
+    </div>
+  </nav>
+   <center><h1><h1><b>About Our Food</b></h1></h1></center> <br>
+   
+     <center><img src="https://www.mcdonalds.com/is/image/content/dam/usa/nfl/assets/hero/About_Our_Food_Hero_1260x560_Desktop.jpg?$Hero_Desktop$"></center> <br><br>
+   
+ 
+        <center><h1> <b>We’re Passionate About Our Food</b></h1></center>
+        <center>From adding more balanced options to our Happy Meal®, to serving up fresh beef Quarter Pounder® burgers that are cooked when you order, we’re always finding ways to show our commitment to our customers and our food. </center><br><br>
+       
+              <center><img src="https://www.mcdonalds.com/is/image/content/dam/usa/nfl/assets/images/2_Pub_Commitment_574x384.jpg?$Publication_Two_Column_Desktop$">
+                <h1><b>Commitment to Quality</b></h1>
+                We're dedicated to improving the way we prepare our quality food and the ingredients that go into it.
+              </center><br><br>
+            
+             <center><img src="https://www.mcdonalds.com/is/image/content/dam/usa/nfl/assets/images/2_Pub_Food_Questions_574x384.jpg?$Publication_Two_Column_Desktop$">
+              <h1><b>Our Food, Your Questions</b></h1>
+              We're transparent about our food; so when you ask, we tell.
+           </center> <br><br>
+           <center><img src="https://www.mcdonalds.com/is/image/content/dam/usa/nfl/assets/images/2_Pub_Whats_In_Your_Food_574x384.jpg?$Publication_Two_Column_Desktop$">
+            <h1><b>What's in Your Food</b></h1>
+            Find out what makes our ingredients special.
+         </center> <br><br>
+         <center><img src="https://www.mcdonalds.com/is/image/content/dam/usa/nfl/assets/images/Fresh_Beef_2-col_1110x740.jpg?$Publication_Two_Column_Desktop$">
+          <h1><b>Fresh Beef</b></h1>
+          Our Quarter Pounder®* patty is made with 100% fresh beef and cooked right when you order. It’s hot and deliciously juicy and full of flavor.<br>
+
+*Weight before cooking 4 oz. At participating McDonald's. Fresh Beef available at most restaurants in contiguous US. Not available in Alaska, Hawaii, and US Territories.
+       </center> <br><br>
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>    
+</body>
+</html>
---
+++ menu.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Menu</title>
+</head>
+<body>
+  <nav class="navbar navbar-expand-lg navbar-light ">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#"></a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <div class="container-fluid">
+            <a class="navbar-brand" href="#">
+              <img src="https://www.mcdonalds.com/content/dam/usa/nfl/assets/nav/arches-logo_108x108.jpg" alt="" width="140" height="100" class="d-inline-block align-top">
+              
+            </a>
+          </div>
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="menu.php">Menu</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="aboutourfood.php">Aboutus</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="signup.php">SignUp</a>
+          </li>
+        </ul>
+        <form class="d-flex">
+          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-success" type="submit">Search</button>
+        </form>
+      </div>
+    </div>
+  </nav>
+
+    <div class="container">
+        <div class="row">
+            <div class="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                <table class="table table-borderless">
+                 <CENTER><h1><b>Menu</b></h1></CENTER>   
+                    <div class="row row-cols-1 row-cols-md-2 g-4">
+                        <div class="col">
+                          <div class="card ">
+                            <img src="https://www.mcdonalds.com/is/image/content/dam/usa/nfl/assets/deals/2Pub_LargeFries_DealsPage_552x368.jpg?$Publication_Two_Column_Desktop$" class="card-img-top" alt="...">
+                            <div class="card-body">
+                              <h1 class="card-title">$1 Large Fries*</h1>
+                              <p class="card-text"><b>Large Fries mean you still have enough after sharing with friends. Get ’em for $1 — only in our app.</b><br>
+
+                                *Offer Valid thru 6/27/21 at participating McDonald’s. Valid 1x/Week. Refer to McD App for details. Mobile Order & Pay at Participating McDonald’s. McD App download and registration required. ©2021 McDonald’s</p>
+                            </div>
+                          </div>
+                        </div>
+                        <div class="col">
+                          <div class="card">
+                            <img src="https://www.mcdonalds.com/is/image/content/dam/usa/nfl/assets/deals/2Pub_99CentBev_DealsPage_552x368.jpg?$Publication_Two_Column_Desktop$" class="card-img-top" alt="...">
+                            <div class="card-body">
+                              <h1 class="card-title">$0.99 Any Size Premium Roast Coffee or Iced Coffee*</h1>
+                              <p class="card-text"><b>Ice-cold or deliciously hot, get it anytime for just $0.99.</b><br>
+
+                                *Offer Valid thru 6/27/21 at participating McDonald’s. Valid 1x/Day. Refer to McD App For Details. Mobile Order & Pay At Participating McDonald’s. McD App download and registration required. © 2021 McDonald’s</p>
+                            </div>
+                          </div>
+                        </div>
+                        <div class="col">
+                          <div class="card">
+                            <img src="https://www.mcdonalds.com/is/image/content/dam/usa/nfl/assets/deals/2Pub_LargeFries_DealsPage_552x368.jpg?$Publication_Two_Column_Desktop$" class="card-img-top" alt="...">
+                            <div class="card-body">
+                              <h1 class="card-title">Free Fries Taste Better</h1>
+                              <p class="card-text"><b>That's just a fact. Get free medium Fries every Friday with a minimum $1 purchase, only in our App with contactless Mobile Order & Pay.*</b><br>
+
+                                *Offer valid 1x each Friday thru 6/27/21 with minimum purchase of $1 (excluding tax). McD App download and registration required. Offer only available through Mobile Order & Pay. Mobile Order & Pay at participating McDonald's. © 2021 McDonald's</p>
+                            </div>
+                          </div>
+                        </div>
+                        
+                      </div>
+
+                </table>
+            </div>
+      </div>
+  </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>    
+</body>
+</html>
